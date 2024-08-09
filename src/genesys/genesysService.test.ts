f3081f400c7a43b033dcc81b22c02e2406a81e45
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-08-09 11:20:08 +0200

    test(genesysService): fix all tests

--- src/genesys/genesysService.test.ts
+++ src/genesys/genesysService.test.ts
@@ -2,20 +2,13 @@ import '../__mocks__/test-params'
 import { GenesysConnectionsState } from '../constants/GenesysConnectionState'
 import { type CallEvent } from './genesysService'
 
-jest.mock('purecloud-platform-client-v2', () => {
-  return require('../__mocks__/purecloud-platform-client-v2')
-})
-
 let triggerEvent: (event: any) => void
 jest.mock('./notificationsController', () => ({
-  __esModule: true,
-  default: {
-    addSubscription: jest.fn((_topic: string, callback: () => void): void => {
-      triggerEvent = callback
-    }),
-    createChannel: async (): Promise<void> => {
-      await Promise.resolve()
-    }
+  addSubscription: jest.fn((_topic: string, callback: () => void): void => {
+    triggerEvent = callback
+  }),
+  createChannel: async (): Promise<void> => {
+    await Promise.resolve()
   }
 }))
 
@@ -42,12 +35,12 @@ const agentMock = {
   }
 }
 
-let state: any
-let accessToken: string
-
 describe('Genesys service', () => {
   let GenesysService: any
   let PlatformClient: any
+  let pcEnvironment: string
+  let pcConversationId: string
+  let accessToken: string
   let callEvent: CallEvent
 
   beforeEach(async () => {
@@ -55,11 +48,10 @@ describe('Genesys service', () => {
     GenesysService = await import('./genesysService')
     PlatformClient = await import('../__mocks__/purecloud-platform-client-v2')
     jest.resetModules()
+
     // Reset variables
-    state = {
-      pcEnvironment: 'fake-environment',
-      pcConversationId: 'fake-conversation-id'
-    }
+    pcEnvironment = 'fake-environment'
+    pcConversationId = 'fake-conversation-id'
     accessToken = 'fake-access-token'
 
     callEvent = {
@@ -122,36 +114,52 @@ describe('Genesys service', () => {
 
   describe('initialize', () => {
     it('should set the environment', async () => {
-      await GenesysService.initialize(state, accessToken)
-      expect(PlatformClient.ApiClient.instance.setEnvironment).toBeCalledTimes(
-        1
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
       )
-      expect(PlatformClient.ApiClient.instance.setEnvironment).toBeCalledWith(
-        state.pcEnvironment
-      )
+      expect(
+        PlatformClient.ApiClient.instance.setEnvironment
+      ).toHaveBeenCalledTimes(1)
+      expect(
+        PlatformClient.ApiClient.instance.setEnvironment
+      ).toHaveBeenCalledWith(pcEnvironment)
     })
 
     it('should set the access token', async () => {
-      await GenesysService.initialize(state, accessToken)
-      expect(PlatformClient.ApiClient.instance.setAccessToken).toBeCalledTimes(
-        1
-      )
-      expect(PlatformClient.ApiClient.instance.setAccessToken).toBeCalledWith(
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
         accessToken
       )
+      expect(
+        PlatformClient.ApiClient.instance.setAccessToken
+      ).toHaveBeenCalledTimes(1)
+      expect(
+        PlatformClient.ApiClient.instance.setAccessToken
+      ).toHaveBeenCalledWith(accessToken)
     })
   })
 
   describe('fetchAniName', () => {
     it('should recover the aniName', async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const aniName = await GenesysService.fetchAniName()
       expect(aniName).toBe('1234')
     })
 
     it("should return a rejected promise if the conversationId doesn't exist", async () => {
-      state.pcConversationId = 'wrong-conversationId'
-      await GenesysService.initialize(state, accessToken)
+      pcConversationId = 'wrong-conversationId'
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const promise = GenesysService.fetchAniName()
       await expect(promise).rejects.toEqual(
         new Error('Conversation id not found')
@@ -161,7 +169,11 @@ describe('Genesys service', () => {
 
   describe('getAgentName', () => {
     it('should retrieve the own name', async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const agentName = GenesysService.getAgentName()
       expect(agentName).toBe('John')
     })
@@ -174,13 +186,21 @@ describe('Genesys service', () => {
 
   describe('isHeld', () => {
     it("should return 'false' when the call is not on hold", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const hold = await GenesysService.isHeld()
       expect(hold).toBeFalsy()
     })
 
     it("should return 'true' when the call is on hold", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       ;(window as any).testParams.genesysHeld = true
       const hold = await GenesysService.isHeld()
       expect(hold).toBeTruthy()
@@ -189,13 +209,21 @@ describe('Genesys service', () => {
 
   describe('isMuted', () => {
     it("should return 'false' when the call is not muted", async () => {
-      await GenesysService.initialize(state, accessToken)
-      const muted = await GenesysService.isHeld()
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
+      const muted = await GenesysService.isMuted()
       expect(muted).toBeFalsy()
     })
 
     it("should return 'true' when the call is muted", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       ;(window as any).testParams.genesysMuted = true
       const muted = await GenesysService.isMuted()
       expect(muted).toBeTruthy()
@@ -204,13 +232,21 @@ describe('Genesys service', () => {
 
   describe('isDialout', () => {
     it("should return 'true' when the call addressRaw does end with pexip node", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const isDialOut = await GenesysService.isDialOut('fake-node')
       expect(isDialOut).toBeTruthy()
     })
 
     it("should return 'false' when the call addressRaw does not end with pexip node", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const isDialOut = await GenesysService.isDialOut('anything-else')
       expect(isDialOut).toBeFalsy()
     })
@@ -218,14 +254,22 @@ describe('Genesys service', () => {
 
   describe('isCallActive', () => {
     it("should return 'false' when the call is not inactive", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       ;(window as any).testParams.genesysInactive = true
       const active = await GenesysService.isCallActive()
       expect(active).toBeFalsy()
     })
 
     it("should return 'true' when the call is active", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const active = await GenesysService.isCallActive()
       expect(active).toBeTruthy()
     })
@@ -233,7 +277,11 @@ describe('Genesys service', () => {
 
   describe('addHoldListener', () => {
     it("should trigger 'handleHold' when the agent push on hold", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const mockHold = jest.fn()
       GenesysService.addMuteListener(jest.fn())
       GenesysService.addHoldListener(mockHold)
@@ -245,7 +293,11 @@ describe('Genesys service', () => {
 
   describe('addEndCallListener', () => {
     it("should trigger 'handleEndCall' with 'shouldDisconnectAll=true' when the agent disconnects", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const mockEndCall = jest.fn()
       GenesysService.addEndCallListener(mockEndCall)
       callEvent.eventBody.participants[0].state = 'disconnected'
@@ -256,7 +308,11 @@ describe('Genesys service', () => {
     })
 
     it("should trigger 'handleEndCall' with 'shouldDisconnectAll=false' when transfer (blind and consult)", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const mockEndCall = jest.fn()
       GenesysService.addEndCallListener(mockEndCall)
       callEvent.eventBody.participants[0].state = 'disconnected'
@@ -267,7 +323,11 @@ describe('Genesys service', () => {
     })
 
     it("shouldn't trigger 'handleEndCall' with any other disconnect type", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const mockEndCall = jest.fn()
       GenesysService.addEndCallListener(mockEndCall)
       callEvent.eventBody.participants[0].state = 'disconnected'
@@ -280,7 +340,11 @@ describe('Genesys service', () => {
 
   describe('addMuteListener', () => {
     it("should trigger 'handleMuteListener' when the agent mutes the audio", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const mockMute = jest.fn()
       GenesysService.addMuteListener(mockMute)
       callEvent.eventBody.participants[0].muted = true
@@ -291,7 +355,11 @@ describe('Genesys service', () => {
 
   describe('addConnectCallListener', () => {
     it("should trigger 'handleConnectCallListener' when a call is connected", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const mockCallConnect = jest.fn()
       GenesysService.addConnectCallListener(mockCallConnect)
       callEvent.eventBody.participants[0].state =
@@ -303,7 +371,11 @@ describe('Genesys service', () => {
     })
 
     it("shouldn't trigger 'handleConnectCallListener' when the customer is disconnected", async () => {
-      await GenesysService.initialize(state, accessToken)
+      await GenesysService.initialize(
+        pcEnvironment,
+        pcConversationId,
+        accessToken
+      )
       const mockCallConnect = jest.fn()
       GenesysService.addConnectCallListener(mockCallConnect)
       callEvent.eventBody.participants[0].state =
